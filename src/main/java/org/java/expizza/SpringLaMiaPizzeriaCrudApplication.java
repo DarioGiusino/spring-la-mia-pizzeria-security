1da301344bd1ab8c4fe6112b7413c0f6909b9ae6
DarioGiusino <dario.giusino@gmail.com>
2023-06-06 15:43:43 +0200

    created fake users

--- src/main/java/org/java/expizza/SpringLaMiaPizzeriaCrudApplication.java
+++ src/main/java/org/java/expizza/SpringLaMiaPizzeriaCrudApplication.java
@@ -2,6 +2,10 @@ package org.java.expizza;
 
 import java.time.LocalDate;
 
+import org.java.expizza.auth.pojo.Role;
+import org.java.expizza.auth.pojo.User;
+import org.java.expizza.auth.serv.RoleService;
+import org.java.expizza.auth.serv.UserService;
 import org.java.expizza.pojo.Ingredient;
 import org.java.expizza.pojo.Pizza;
 import org.java.expizza.pojo.SpecialOffer;
@@ -12,6 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class SpringLaMiaPizzeriaCrudApplication implements CommandLineRunner {
@@ -23,7 +28,13 @@ public class SpringLaMiaPizzeriaCrudApplication implements CommandLineRunner {
 	private SpecialOfferService specialOfferService;
 	
 	@Autowired
-	private IngredientServ ingredientServ;
+	private IngredientServ ingredientServ; 
+	
+	@Autowired
+	private UserService userService;
+	
+	@Autowired
+	private RoleService roleService;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringLaMiaPizzeriaCrudApplication.class, args);
@@ -46,6 +57,8 @@ public class SpringLaMiaPizzeriaCrudApplication implements CommandLineRunner {
 		ingredientServ.save(i5);
 		ingredientServ.save(i6);
 		
+//		--------------------------------------------------------------------------------------------------------------------
+		
 		Pizza pizza1 = new Pizza("Margherita", "La bella Margherita", "https://picsum.photos/200/300", 9.99f, i1, i3);
 		Pizza pizza2 = new Pizza("Diavola", "La bella Diavola", "https://picsum.photos/200/300", 10.99f, i2, i4);
 		Pizza pizza3 = new Pizza("Napoli", "La bella Napoli", "https://picsum.photos/200/300", 11.99f, i5, i6);
@@ -57,6 +70,8 @@ public class SpringLaMiaPizzeriaCrudApplication implements CommandLineRunner {
 		pizzaService.save(pizza3);
 		pizzaService.save(pizza4);
 		pizzaService.save(pizza5);
+		
+//		--------------------------------------------------------------------------------------------------------------------
 		
 		SpecialOffer so1 = new SpecialOffer("Special Offer 1", LocalDate.parse("2023-01-01"), LocalDate.parse("2023-10-01"), 10, pizza1);
 		SpecialOffer so2 = new SpecialOffer("Special Offer 2", LocalDate.parse("2023-02-01"), LocalDate.parse("2023-09-01"), 20, pizza2);
@@ -75,5 +90,23 @@ public class SpringLaMiaPizzeriaCrudApplication implements CommandLineRunner {
 		specialOfferService.save(so6);
 		specialOfferService.save(so7);
 		specialOfferService.save(so8);
+		
+//		--------------------------------------------------------------------------------------------------------------------
+		
+		Role userRole = new Role("USER");
+		Role adminRole = new Role("ADMIN");
+		
+		roleService.save(userRole);
+		roleService.save(adminRole);
+		
+//		--------------------------------------------------------------------------------------------------------------------
+		
+		final String psw = new BCryptPasswordEncoder().encode("password");
+		
+		User userUser = new User("user", psw, userRole);
+		User userAdmin = new User("admin", psw, adminRole);
+				
+		userService.save(userUser);
+		userService.save(userAdmin);
 	}
 }
